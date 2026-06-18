26e0ebecb19e4a2505e64902299162e3da4b4c94
lukshyaverma <67636863+lukshyaverma@users.noreply.github.com>
2026-06-18 16:33:49 +0530

    Update ML Ideas for Fraud.py

--- ML Ideas for Fraud.py
+++ ML Ideas for Fraud.py
@@ -1,3 +1,12 @@
 18/06/2026
 
-Datasets
+Datasets:
+  1. Datacamp
+    a. Datapoints - 3,39,606
+    b. Features - 15 (including key)
+    c. Fraud Rate - 52 bps (1782)
+  2. Kaggle
+    a. Datapoints - 5,55,719
+    b. Features - 23 (including key)
+    c. Fraud Rate - 39 bps (2145)
+    
