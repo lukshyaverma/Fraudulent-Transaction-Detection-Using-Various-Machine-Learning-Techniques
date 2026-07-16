da3b8ec176cdd7957a5b9159bd6326f22d50070a
lukshyaverma <67636863+lukshyaverma@users.noreply.github.com>
2026-07-16 15:08:42 +0530

    Create 12_velocity_var.sql

---
+++ 12_velocity_var.sql
@@ -0,0 +1,32 @@
+DROP TABLE IF EXISTS cm_velocity;
+CREATE TABLE cm_velocity AS
+WITH base AS (
+    SELECT
+        txn_key,
+        customer_id,
+        unix_time,
+        COUNT(*) OVER (
+            PARTITION BY customer_id
+            ORDER BY unix_time
+            RANGE BETWEEN 86400 PRECEDING AND 1 PRECEDING
+        ) AS cust_txn_cnt_24h
+    FROM main_data
+)
+SELECT
+    txn_key,
+    customer_id,
+    unix_time,
+    cust_txn_cnt_24h,
+    AVG(cust_txn_cnt_24h) OVER (
+        PARTITION BY customer_id
+        ORDER BY unix_time
+        RANGE BETWEEN 2592000 PRECEDING AND 1 PRECEDING
+    ) AS cust_txn_cnt_24h_avg_30d,
+    cust_txn_cnt_24h / NULLIF(
+        AVG(cust_txn_cnt_24h) OVER (
+            PARTITION BY customer_id
+            ORDER BY unix_time
+            RANGE BETWEEN 2592000 PRECEDING AND 1 PRECEDING
+        ), 0
+    ) AS cust_velocity_ratio_30d
+FROM base;
