56824af163ea9c364f50697a195cc77d9ff24820
lukshyaverma <67636863+lukshyaverma@users.noreply.github.com>
2026-07-16 13:16:30 +0530

    Create 09_ftg_vars_2.sql

---
+++ 09_ftg_vars_2.sql
@@ -0,0 +1,31 @@
+DROP TABLE IF EXISTS merch_ftg;
+CREATE TABLE merch_ftg AS
+SELECT
+    txn_key,
+    merchant_id,
+    unix_time,
+    COALESCE(SUM(fraud_ind) OVER w, 0) AS merch_frd_txn_30d,
+    COUNT(*) OVER w AS merch_tot_txn_30d,
+    COALESCE((SUM(fraud_ind) OVER w / NULLIF(COUNT(*) OVER w, 0)),0) * 100 AS merch_ftg_30d
+FROM main_data
+WINDOW w AS (
+    PARTITION BY merchant_id
+    ORDER BY unix_time
+    RANGE BETWEEN 2592000 PRECEDING AND 1 PRECEDING
+);
+
+DROP TABLE IF EXISTS cat_ftg;
+CREATE TABLE cat_ftg AS
+SELECT
+    txn_key,
+    category,
+    unix_time,
+    COALESCE(SUM(fraud_ind) OVER w, 0) AS cat_frd_txn_30d,
+    COUNT(*) OVER w AS cat_tot_txn_30d,
+    COALESCE((SUM(fraud_ind) OVER w / NULLIF(COUNT(*) OVER w, 0)),0) * 100 AS cat_ftg_30d
+FROM main_data
+WINDOW w AS (
+    PARTITION BY category
+    ORDER BY unix_time
+    RANGE BETWEEN 2592000 PRECEDING AND 1 PRECEDING
+);
